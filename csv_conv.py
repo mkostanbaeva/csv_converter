4a36473bc37bbf13f78810897c95a0f0f8abac77
mkostanbaeva <maria.kostanbaeva@gmail.com>
2016-11-07 18:36:03 +0300

    Update csv_conv.py

--- csv_conv.py
+++ csv_conv.py
@@ -6,7 +6,10 @@ import requests
 #from Tkinter import Tk
 #from tkFileDialog import askopenfilename, asksaveasfilename
 import re
+import sys
 
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 def load_category_csv():
     result = dict()
@@ -37,6 +40,11 @@ brands_dict = load_brands()
 
 regex = re.compile('\(.*?\)')
 
+def floatx(value):
+  return float(value or 0.0)
+
+def strx(value):
+  return str(value or '')
 
 def get_output_row(irow, is_sup):
     orow = dict()
@@ -52,145 +60,145 @@ def get_output_row(irow, is_sup):
 
     # 5-Наименование
     if irow['name']:
-        vals = irow['name'].value.split(u':')
+        vals = irow['name'].split(u':')
         if is_sup:
-            orow['namepr'].value = vals[0].capitalize()
-            orow['model'].value = vals[1]
+            orow['namepr'] = vals[0].capitalize()
+            orow['model'] = vals[1]
         elif len(vals) >= 3:
-            orow['namepr'].value = vals[0].capitalize()
-            orow['model'].value = vals[2]
+            orow['namepr'] = vals[0].capitalize()
+            orow['model'] = vals[2]
         else:
-            orow['model'].value = irow['name'].value
+            orow['namepr'] = irow['name']
 
     # 4-Категория
-    if irow['groupproduct'].value:
-        id_erp = re.sub(regex, '', irow['groupproduct'].value)
+    if irow['groupproduct']:
+        id_erp = re.sub(regex, '', irow['groupproduct'])
         if id_erp in category_dict:
-            orow['catid'].value = category_dict[id_erp]
+            orow['catid'] = category_dict[id_erp]
         else:
-            orow['catid'].value = irow['groupproduct'].value
+            orow['catid'] = irow['groupproduct']
 
     # 6-Бренд
-    if irow['brandname'].value:
-        brand = re.sub(regex, '', irow['brandname'].value)
+    if irow['brandname']:
+        brand = re.sub(regex, '', irow['brandname'])
         if brand and brand.replace(' ', '').lower() in brands_dict:
-            orow['brandpr'].value = brands_dict[brand.replace(' ', '').lower()]
+            orow['brandpr']= brands_dict[brand.replace(' ', '').lower()]
         else:
-            orow['brandpr'].value = irow['brandname'].value
+            orow['brandpr'] = irow['brandname']
 
     # 10-пол
-    if irow['sex'].value:
-        if irow['sex'].value.strip().lower() == u'f':
-            orow['sexpr'].value = u'ж'
-        elif irow['sex'].value.strip().lower() == u'm':
-            orow['sexpr'].value = u'м'
-        elif irow['sex'].value.strip().lower() == u'u':
-            orow['sexpr'].value = ' '
+    if irow['sex']:
+        if irow['sex'].strip().lower() == u'f':
+            orow['sexpr'] = u'ж'
+        elif irow['sex'].strip().lower() == u'm':
+            orow['sexpr'] = u'м'
+        elif irow['sex'].strip().lower() == u'u':
+            orow['sexpr'] = ' '
         else:
-            orow['sexpr'].value = irow['sex'].value
+            orow['sexpr'] = irow['sex']
 
     # 16-состав товара
-    irow['structpr'].value = irow['productstr'].value
+    irow['structpr'] = irow['productstr']
 
     # 18-Возраст от
-    if irow['agefromid'].value:
-        if irow['agefromid'].value.strip().lower() == u'0M+':
-            orow['agefrompr'].value = u'0'
-        elif irow['agefromid'].value.strip().lower() == u'3M+':
-            orow['agefrompr'].value = u'3'
-        elif irow['agefromid'].value.strip().lower() == u'6M+':
-            orow['agefrompr'].value = u'6'
-        elif irow['agefronid'].value.strip().lower() == u'9M+':
-            orow['agefrompr'].value = u'9'
-        elif irow['agefromid'].value.strip().lower() == u'1,5Y+':
-            orow['agefrompr'].value = u'18'
-        elif irow['agefromid'].value.strip().lower() == u'10Y+':
-            orow['agefrompr'].value = u'120'
-        elif irow['agefronid'].value.strip().lower() == u'11Y+':
-            orow['agefrompr'].value = u'132'
-        elif irow['agefromid'].value.strip().lower() == u'12Y+':
-            orow['agefrompr'].value = u'144'
-        elif irow['agefromid'].value.strip().lower() == u'1Y+':
-            orow['agefrompr'].value = u'12'
-        elif irow['agefromid'].value.strip().lower() == u'2Y+':
-            orow['agefrompr'].value = u'24'
-        elif irow['agefromid'].value.strip().lower() == u'3Y+':
-            orow['agefrompr'].value = u'36'
-        elif irow['agefromid'].value.strip().lower() == u'5Y+':
-            orow['agefrompr'].value = u'60'
-        elif irow['agefromid'].value.strip().lower() == u'6Y+':
-            orow['agefrompr'].value = u'72'
-        elif irow['agefromid'].value.strip().lower() == u'7Y+':
-            orow['agefrompr'].value = u'84'
-        elif irow['agefromid'].value.strip().lower() == u'8Y+':
-            orow['agefrompr'].value = u'96'
-        elif irow['agefromid'].value.strip().lower() == u'9Y+':
-            orow['agefrompr'].value = u'108'
-        elif irow['agefromid'].value.strip().lower() == u'ADULT':
-            orow['agefrompr'].value = u' '
+    if irow['agefromid']:
+        if irow['agefromid'].lower() == '0m+':
+            orow['agefrompr'] = '0'
+        elif irow['agefromid'].lower() == '3m+':
+            orow['agefrompr'] = '3'
+        elif irow['agefromid'].lower() == '6m+':
+            orow['agefrompr'] = '6'
+        elif irow['agefromid'].lower() == '9m+':
+            orow['agefrompr'] = '9'
+        elif irow['agefromid'].lower() == '1,5y+':
+            orow['agefrompr'] = '18'
+        elif irow['agefromid'].lower() == '10y+':
+            orow['agefrompr'] = '120'
+        elif irow['agefromid'].lower() == '11y+':
+            orow['agefrompr'] = '132'
+        elif irow['agefromid'].lower() == '12y+':
+            orow['agefrompr'] = '144'
+        elif irow['agefromid'].lower() == '1y+':
+            orow['agefrompr'] = '12'
+        elif irow['agefromid'].lower() == '2y+':
+            orow['agefrompr'] = '24'
+        elif irow['agefromid'].lower() == '3y+':
+            orow['agefrompr'] = '36'
+        elif irow['agefromid'].lower() == '5y+':
+            orow['agefrompr'] = '60'
+        elif irow['agefromid'].lower() == '6y+':
+            orow['agefrompr'] = '72'
+        elif irow['agefromid'].lower() == '7y+':
+            orow['agefrompr'] = '84'
+        elif irow['agefromid'].lower() == '8y+':
+            orow['agefrompr'] = '96'
+        elif irow['agefromid'].lower() == '9y+':
+            orow['agefrompr'] = '108'
+        elif irow['agefromid'].lower() == 'adult':
+            orow['agefrompr'] = ' '
         else:
-            orow['agefrompr'].value = irow['agefromid'].value
+            orow['agefrompr'] = irow['agefromid']
 
     # 19-Возраст до
-    if irow['agetoid'].value:
-        if irow['agetoid'].value.strip().lower() == u'3M-':
-            orow['agetopr'].value = u'3'
-        elif irow['agetoid'].value.strip().lower() == u'6M-':
-            orow['agetopr'].value = u'6'
-        elif irow['agetoid'].value.strip().lower() == u'9M-':
-            orow['agetopr'].value = u'9'
-        elif irow['agetoid'].value.strip().lower() == u'1,5Y-':
-            orow['agetopr'].value = u'18'
-        elif irow['agetoid'].value.strip().lower() == u'10Y-':
-            orow['agetopr'].value = u'120'
-        elif irow['agetoid'].value.strip().lower() == u'11Y-':
-            orow['agetopr'].value = u'132'
-        elif irow['agetoid'].value.strip().lower() == u'12Y-':
-            orow['agetopr'].value = u'144'
-        elif irow['agetoid'].value.strip().lower() == u'1Y-':
-            orow['agetopr'].value = u'12'
-        elif irow['agetoid'].value.strip().lower() == u'2Y-':
-            orow['agetopr'].value = u'24'
-        elif irow['agetoid'].value.strip().lower() == u'3Y-':
-            orow['agetopr'].value = u'36'
-        elif irow['agetoid'].value.strip().lower() == u'4Y-':
-            orow['agetopr'].value = u'48'
-        elif irow['agetoid'].value.strip().lower() == u'5Y-':
-            orow['agetopr'].value = u'60'
-        elif irow['agetoid'].value.strip().lower() == u'6Y-':
-            orow['agetopr'].value = u'72'
-        elif irow['agetoid'].value.strip().lower() == u'7Y-':
-            orow['agetopr'].value = u'84'
-        elif irow['agetoid'].value.strip().lower() == u'8Y-':
-            orow['agetopr'].value = u'96'
-        elif irow['agetoid'].value.strip().lower() == u'9Y-':
-            orow['agetopr'].value = u'108'
+    if irow['agetoid']:
+        if irow['agetoid'].lower() == '3m-':
+            orow['agetopr'] = '3'
+        elif irow['agetoid'].lower() == '6m-':
+            orow['agetopr'] = '6'
+        elif irow['agetoid'].lower() == '9m-':
+            orow['agetopr'] = '9'
+        elif irow['agetoid'].lower() == '1,5y-':
+            orow['agetopr'] = '18'
+        elif irow['agetoid'].lower() == '10y-':
+            orow['agetopr'] = '120'
+        elif irow['agetoid'].lower() == '11y-':
+            orow['agetopr'] = '132'
+        elif irow['agetoid'].lower() == '12y-':
+            orow['agetopr'] = '144'
+        elif irow['agetoid'].lower() == '1y-':
+            orow['agetopr'] = '12'
+        elif irow['agetoid'].lower() == '2y-':
+            orow['agetopr'] = '24'
+        elif irow['agetoid'].lower() == '3y-':
+            orow['agetopr'] = '36'
+        elif irow['agetoid'].lower() == '4y-':
+            orow['agetopr'] = '48'
+        elif irow['agetoid'].lower() == '5y-':
+            orow['agetopr'] = '60'
+        elif irow['agetoid'].lower() == '6y-':
+            orow['agetopr'] = '72'
+        elif irow['agetoid'].lower() == '7y-':
+            orow['agetopr'] = '84'
+        elif irow['agetoid'].lower() == '8y-':
+            orow['agetopr'] = '96'
+        elif irow['agetoid'].lower() == '9y-':
+            orow['agetopr'] = '108'
         else:
-            orow['agetopr'].value = irow['agetoid'].value
+            orow['agetopr'] = irow['agetoid']
 
     #24-Коллекция
-    orow['seasonpr'].value = irow['season'].value + irow['seasonyear'].value
+    orow['seasonpr'] = strx(irow['season']) + strx(irow['seasonyear'])
 
     #17 - Страна происхождения
-    orow['countrypr'].value = irow['countryname'].value
+    orow['countrypr'] = irow['countryname']
 
     #12 - Ширина
-    orow['wightpr'].value = irow['wight'].value * 100
+    orow['wightpr'] = floatx(irow['wight']) * 100.0
 
     #14 - Высота
-    orow['heightpr'].value = irow['height'].value * 100
+    orow['heightpr'] = floatx(irow['height']) * 100.0
 
     #13 - Длина
-    orow['lenghtpr'] = irow['lenght'].value * 100
+    orow['lenghtpr'] = floatx(irow['lenght']) * 100.0
 
     #15 - Вес
-    orow['weightpr'].value = irow['weight'].value * 1000
+    orow['weightpr'] = floatx(irow['weight'])  * 1000.0
 
     #11-Габариты
-    orow['volpr'].value = unicode('{0:.0f}'.format(float(irow['wight'].value) * 100)) + u'x' + unicode('{0:.0f}'.format(float(irow['lenght'].value )* 100.0)) + u'x' + unicode('{0:.0f}'.format(float(irow['height'].value) * 100.0))
+    orow['volpr'] = str(floatx(irow['wight']) * 100) + 'x' + str(floatx(irow['lenght'] )* 100.0) + 'x' + str(floatx(irow['height']) * 100.0)
 
     #9 - Описание
-    orow['descrpr'].value = irow['description'].value
+    orow['descrpr'] = irow['description']
 
     return orow
 
@@ -198,13 +206,13 @@ def get_output_row(irow, is_sup):
 # Copy
 i = 1
 with open('output.csv', 'wb') as writecsvfile:
-    out_fieldnames = ['index', 'iddm', 'art', 'catid', 'namepr', 'brandpr', 'model', 'colorpr', 'descrpr', 'sexpr', 'volpr', 'wightpr', 'lenghtpr', 'heightpr', 'wightpr', 'structpr', 'countrypr', 'agefronpr', 'agetopr', 'kgt', 'recomm', 'tags', 'rating', 'seasonpr', 'price', 'pricevol']
+    out_fieldnames = ['index', 'iddm', 'art', 'catid', 'namepr', 'brandpr', 'model', 'colorpr', 'descrpr', 'sexpr', 'volpr', 'wightpr', 'lenghtpr', 'heightpr', 'weightpr', 'structpr', 'countrypr', 'agefrompr', 'agetopr', 'kgt', 'recomm', 'tags', 'rating', 'seasonpr', 'price', 'pricevol']
     writer = csv.DictWriter(writecsvfile, fieldnames=out_fieldnames, delimiter=',')
     writer.writeheader()
 
     with open('input.csv', 'rb') as readcsvfile:
         in_fieldnames = ['iddm', 'art', 'name', 'brandid','brandname', 'countryid', 'countryname', '1', '2', 'weight', 'weightid','lenght', 'wight', 'height', 'volume', 'createdate', 'sex', 'productstr', 'groupproduct', 'namegroupproduct', 'colorid', 'colorname', 'sizeid', 'sizename', 'agefromid', 'agefromname', 'agetoid', 'agetoname', 'season', 'seasonyear', 'description']
-        reader = csv.DictReader(readcsvfile, fieldnames=in_fieldnames, delimiter=',')
+        reader = csv.DictReader(readcsvfile, fieldnames=in_fieldnames, delimiter='|')
         for irow in reader:
             if unicode(irow['iddm'])[-3:] == u'001':
                 orow = get_output_row(irow, True)
@@ -219,32 +227,32 @@ with open('output.csv', 'wb') as writecsvfile:
             writer.writerow(orow)
 
 
-orow['index'].value = u'н/п'
-orow['iddm'].value = u'уникальный ID ДМ'
-orow['art'].value = u'Артикул'
-orow['catid'].value = u'Категория'
-orow['namepr'].value = u'Наименование'
-orow['brandpr'].value = u'Бренд'
-orow['model'].value = u'Модель'
-orow['colorpr'].value = u'Цвет'
-orow['descrpr'].value = u'Описание'
-orow['sexpr'].value = u'Пол'
-orow['volpr'].value = u'Габариты'
-orow['wightpr'].value = u'Ширина см'
-orow['lenghtpr'].value = u'Длина см'
-orow['heightpr'].value = u'Высота см'
-orow['weightpr'].value = u'Вес'
-orow['structpr'].value = u'Материал'
-orow['countrypr'].value = u'Страна производитель'
-orow['agefrompr'].value = u'Возрастная группа от'
-orow['agetopr'].value = u'Возрастная группа до'
-orow['kgt'].value = u'КГТ'
-orow['recomm'].value = u'Рекомендуем?'
-orow['tags'].value = u'теги, через запятую'
-orow['rating'].value = u'Рейтинг'
-orow['seasonpr'].value = u'Коллекция'
-orow['price'].value = u'Цена'
-orow['pricevol'].value = u'Ценовой уровень'
+orow['index'] = u'н/п'
+orow['iddm'] = u'уникальный ID ДМ'
+orow['art'] = u'Артикул'
+orow['catid'] = u'Категория'
+orow['namepr'] = u'Наименование'
+orow['brandpr'] = u'Бренд'
+orow['model'] = u'Модель'
+orow['colorpr'] = u'Цвет'
+orow['descrpr'] = u'Описание'
+orow['sexpr'] = u'Пол'
+orow['volpr'] = u'Габариты'
+orow['wightpr'] = u'Ширина см'
+orow['lenghtpr'] = u'Длина см'
+orow['heightpr'] = u'Высота см'
+orow['weightpr'] = u'Вес'
+orow['structpr'] = u'Материал'
+orow['countrypr'] = u'Страна производитель'
+orow['agefrompr'] = u'Возрастная группа от'
+orow['agetopr'] = u'Возрастная группа до'
+orow['kgt'] = u'КГТ'
+orow['recomm'] = u'Рекомендуем?'
+orow['tags'] = u'теги, через запятую'
+orow['rating'] = u'Рейтинг'
+orow['seasonpr'] = u'Коллекция'
+orow['price'] = u'Цена'
+orow['pricevol'] = u'Ценовой уровень'
 
 # Save the file
 #wb.save("result.xlsx")
